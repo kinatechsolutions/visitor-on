8e4f06f11c698f3383f6512d3f5fc2f0afd70391
Kinare <michaelkinare@gmail.com>
2025-02-23 16:19:35 +0300

    port change

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [
     vue(),
     VitePWA({
