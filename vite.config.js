7f272e310fec47315523827290e23743f72be6eb
Kinare <michaelkinare@gmail.com>
2025-02-23 16:14:11 +0300

    gh pages confi

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-server: {
-port: 3000
-},
-plugins: [
+  server: {
+    port: 3000,
+  },
+  plugins: [
     vue(),
     VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-    manifest: {
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
         name: 'Visitor On Landing',
         short_name: 'VOL',
         description: 'Visitor On Landing Application',
         theme_color: '#ffffff',
         icons: [
-        {
+          {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-        },
-        {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-        },
-        {
+            type: 'image/png',
+          },
+          {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
-        }
-        ]
-    }
-    })
-],
-})
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+
+  base: '/visitor-on/', // Replace with your GitHub repository name
+});
